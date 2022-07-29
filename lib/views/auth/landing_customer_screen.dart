357d846fb50b6295c4771bfcae71c4b50f6f1e11
yooni hou <yooni.hou@globeunion.com>
2022-07-29 18:16:36 +0800

    feat: loginPage input & bottom text

--- lib/views/auth/landing_customer_screen.dart
+++ lib/views/auth/landing_customer_screen.dart
@@ -5,140 +5,183 @@ class LandingCustomerScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+        resizeToAvoidBottomInset: false,
         body: Padding(
-      padding: const EdgeInsets.all(8.0),
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          padding: const EdgeInsets.all(8.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              const Text(
-                "Create Customer's Account",
-                style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  const Text(
+                    "Create Customer's Account",
+                    style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold),
+                  ),
+                  IconButton(
+                    onPressed: () {},
+                    icon: const Icon(
+                      Icons.person,
+                      size: 35,
+                      color: Color.fromARGB(255, 235, 194, 80),
+                    ),
+                  ),
+                ],
               ),
-              IconButton(
-                onPressed: () {},
-                icon: const Icon(
-                  Icons.person,
-                  size: 35,
-                  color: Color.fromARGB(255, 235, 194, 80),
-                ),
-              ),
-            ],
-          ),
-          Row(
-            children: [
-              const CircleAvatar(
-                radius: 60,
-                backgroundColor: Color.fromARGB(255, 235, 194, 80),
+              Row(
+                children: [
+                  const CircleAvatar(
+                    radius: 60,
+                    backgroundColor: Color.fromARGB(255, 235, 194, 80),
+                  ),
+                  const SizedBox(
+                    width: 10,
+                  ),
+                  Column(
+                    children: [
+                      Container(
+                        decoration: const BoxDecoration(
+                          color: Color.fromARGB(255, 235, 194, 80),
+                          borderRadius: BorderRadius.only(
+                              topLeft: Radius.circular(15),
+                              topRight: Radius.circular(15)),
+                        ),
+                        child: IconButton(
+                          onPressed: () {},
+                          icon: const Icon(
+                            Icons.camera_alt,
+                            color: Colors.white,
+                          ),
+                        ),
+                      ),
+                      const SizedBox(
+                        height: 10,
+                      ),
+                      Container(
+                        decoration: const BoxDecoration(
+                          color: Color.fromARGB(255, 235, 194, 80),
+                          borderRadius: BorderRadius.only(
+                              bottomLeft: Radius.circular(15),
+                              bottomRight: Radius.circular(15)),
+                        ),
+                        child: IconButton(
+                          onPressed: () {},
+                          icon: const Icon(
+                            Icons.photo,
+                            color: Colors.white,
+                          ),
+                        ),
+                      ),
+                    ],
+                  )
+                ],
               ),
               const SizedBox(
-                width: 10,
+                height: 15,
               ),
               Column(
                 children: [
-                  Container(
-                    decoration: const BoxDecoration(
-                      color: Color.fromARGB(255, 235, 194, 80),
-                      borderRadius: BorderRadius.only(
-                          topLeft: Radius.circular(15),
-                          topRight: Radius.circular(15)),
-                    ),
-                    child: IconButton(
-                      onPressed: () {},
-                      icon: const Icon(
-                        Icons.camera_alt,
-                        color: Colors.white,
+                  TextFormField(
+                    decoration: InputDecoration(
+                      labelText: 'Full Name',
+                      hintText: 'Enter your full name',
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(25),
                       ),
                     ),
                   ),
                   const SizedBox(
-                    height: 10,
+                    height: 15,
                   ),
-                  Container(
-                    decoration: const BoxDecoration(
-                      color: Color.fromARGB(255, 235, 194, 80),
-                      borderRadius: BorderRadius.only(
-                          bottomLeft: Radius.circular(15),
-                          bottomRight: Radius.circular(15)),
-                    ),
-                    child: IconButton(
-                      onPressed: () {},
-                      icon: const Icon(
-                        Icons.photo,
-                        color: Colors.white,
+                  TextFormField(
+                    decoration: InputDecoration(
+                      labelText: 'Email',
+                      hintText: 'Enter your Email',
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(25),
                       ),
                     ),
+                  ),
+                  const SizedBox(
+                    height: 15,
+                  ),
+                  TextFormField(
+                    decoration: InputDecoration(
+                      labelText: 'Email',
+                      hintText: 'Enter your Email',
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(25),
+                      ),
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 15,
+                  ),
+                  // ignore: sized_box_for_whitespace
+                  Container(
+                    width: MediaQuery.of(context).size.width - 40,
+                    height: 50,
+                    decoration: BoxDecoration(
+                      color: const Color.fromARGB(255, 235, 194, 80),
+                      borderRadius: BorderRadius.circular(25),
+                    ),
+                    child: const Center(
+                      child: Text(
+                        'Sign up',
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 18,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ),
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      const Text(
+                        'Aleady have an account?',
+                        style: TextStyle(
+                            fontSize: 14, fontWeight: FontWeight.w500),
+                      ),
+                      TextButton(
+                        onPressed: () {},
+                        child: const Text(
+                          'Login',
+                          style: TextStyle(
+                            color: Color.fromARGB(255, 235, 194, 80),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                  const Text(
+                    'Or',
+                    style: TextStyle(fontSize: 12, fontWeight: FontWeight.w500),
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      const Text(
+                        "Create a seller's Account?",
+                        style: TextStyle(
+                            fontSize: 14, fontWeight: FontWeight.w500),
+                      ),
+                      TextButton(
+                        onPressed: () {},
+                        child: const Text(
+                          'Sing up',
+                          style: TextStyle(
+                            color: Color.fromARGB(255, 235, 194, 80),
+                          ),
+                        ),
+                      ),
+                    ],
                   ),
                 ],
               )
             ],
           ),
-          const SizedBox(
-            height: 15,
-          ),
-          Column(
-            children: [
-              TextFormField(
-                decoration: InputDecoration(
-                  labelText: 'Full Name',
-                  hintText: 'Enter your full name',
-                  border: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(25),
-                  ),
-                ),
-              ),
-              const SizedBox(
-                height: 15,
-              ),
-              TextFormField(
-                decoration: InputDecoration(
-                  labelText: 'Email',
-                  hintText: 'Enter your Email',
-                  border: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(25),
-                  ),
-                ),
-              ),
-              const SizedBox(
-                height: 15,
-              ),
-              TextFormField(
-                decoration: InputDecoration(
-                  labelText: 'Email',
-                  hintText: 'Enter your Email',
-                  border: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(25),
-                  ),
-                ),
-              ),
-              const SizedBox(
-                height: 15,
-              ),
-              // ignore: sized_box_for_whitespace
-              Container(
-                width: MediaQuery.of(context).size.width - 40,
-                height: 50,
-                decoration: BoxDecoration(
-                  color: const Color.fromARGB(255, 235, 194, 80),
-                  borderRadius: BorderRadius.circular(25),
-                ),
-                child: const Center(
-                  child: Text(
-                    'Sign up',
-                    style: TextStyle(
-                      color: Colors.white,
-                      fontSize: 18,
-                      fontWeight: FontWeight.bold,
-                    ),
-                  ),
-                ),
-              )
-            ],
-          )
-        ],
-      ),
-    ));
+        ));
   }
 }
