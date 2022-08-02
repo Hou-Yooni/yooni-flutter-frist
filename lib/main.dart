360bb2d77d5174436fb614a969a18c7d0809cf53
yooni hou <yooni.hou@globeunion.com>
2022-08-02 18:00:41 +0800

    feat: theme primarySwatch color

--- lib/main.dart
+++ lib/main.dart
@@ -2,8 +2,25 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_project/views/auth/landing_customer_screen.dart';
 
-void main() async{
-  WidgetsFlutterBinding.ensureInitialized(); //如果 Flutter 需要在調用 runApp 之前調用原生代碼都 需要加上這一行。
+const MaterialColor yellowColor = MaterialColor(
+  0xFFFDD835,
+  <int, Color>{
+    50: Color(0xFFFFFDE7),
+    100: Color(0xFFFFFDE7),
+    200: Color(0xFFFFFDE7),
+    300: Color(0xFFFFF9C4),
+    400: Color(0xFFFFF59D),
+    500: Color(0xFFFFF176),
+    600: Color(0xFFFFEE58),
+    700: Color(0xFFFDD835),
+    800: Color(0xFFFBC02D),
+    900: Color(0xFFF9A825),
+  },
+);
+
+void main() async {
+  WidgetsFlutterBinding
+      .ensureInitialized(); //如果 Flutter 需要在調用 runApp 之前調用原生代碼都 需要加上這一行。
   // ignore: avoid_print
   await Firebase.initializeApp().then((value) => print('Firebase initialized'));
   runApp(const MyApp());
@@ -18,10 +35,8 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
         debugShowCheckedModeBanner: false,
         title: 'Flutter Demo',
-        theme: ThemeData(
-          primarySwatch: Colors.blue,
-          fontFamily: 'Dosis-Regular'
-        ),
+        theme:
+            ThemeData(primarySwatch: yellowColor, fontFamily: 'Dosis-Regular'),
         home: LandingCustomerScreen());
   }
 }
